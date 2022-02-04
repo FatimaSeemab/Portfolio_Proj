8192402dbe5cbcf2c1f0be44d4ce5a5825dbdbeb
FatimaSeemab <fseemab.bscs19seecs@seecs.edu.pk>
2022-02-05 00:42:39 +0500

    main removed

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head> */}
-        <main>
+        {/* <main> */}
           <Nav />
           <Main />
           <div className='bg-gray-600'><AboutMe /></div>
@@ -29,7 +29,7 @@ export default function Home() {
           {/* </section> */}
           {/* <Certificates /> */}
           <Contact />
-        </main>
+        {/* </main> */}
       
     </div>
   )
