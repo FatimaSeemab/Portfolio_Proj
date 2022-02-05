47e26aed65638c45f08c5f8f65c72524f16516e7
FatimaSeemab <fseemab.bscs19seecs@seecs.edu.pk>
2022-02-05 10:45:29 +0500

    xfcv

--- Components/AboutMe.tsx
+++ Components/AboutMe.tsx
@@ -5,7 +5,7 @@ function AboutMe() {
 	   <br />
 	  <div className='mx-5 border-2 shadow-lg hover:shadow-lg  border-sky-700 font-serif bg-gray-200'>
 		  <h2 className='m-2 text-4xl font-bold text-blue-900'>About Me</h2>
-		  <p className='m-2 text-xl text-blue-500'>I am in 6th semester of bachelors in Computer Science, having great interest in web development of React Js, having an experience of internship of three months at Xypher Technologies,some personal and university projects related to web development.I am very much enthusiastic in learning block chain and web3.0.</p>
+		  <p className='m-2 text-xl text-blue-500'>I am in 6th semester of bachelors in Computer Science, having experience of web development in React Js,done an  internship of three months at Xypher Technologies,some personal and university projects related to web development.I am  enthusiastic to learn block chain and web3.0.</p>
 	  </div>
 	  <br />
   </div>;
