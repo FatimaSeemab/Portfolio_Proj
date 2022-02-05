4bdbbca5360e45ed611322ce5f442e90ecc71d2f
FatimaSeemab <fseemab.bscs19seecs@seecs.edu.pk>
2022-02-05 11:44:12 +0500

    fonts set

--- Components/Main.tsx
+++ Components/Main.tsx
@@ -5,12 +5,12 @@ function Main() {
   backgroundSize: 'cover', 
   backgroundPosition: 'center center',
   backgroundRepeat: 'no-repeat',}}>
-	  <div className='font-serif md:h-[35rem] h-[25rem] flex justify-center items-center'>
+	  <div className='font-serif md:h-[35rem] h-[45rem] flex justify-center items-center'>
 		 <div>
-		 <div className='text-center  lg:text-4xl sm:text-lg'>Hi! I am<span className='font-semibold'> Fatima Seemab </span></div>
-		  <div className='text-center md:text-2xl sm:text-lg'>Student of Computer Science</div>
-		  <div className="text-center md:text-2xl sm:text-lg">BS from National University of Science and Technology (NUST)</div>
-		  <div className="text-center md:text-2xl sm:text-lg font-semibold">Frontend Developer</div></div>
+		 <div className='text-center  lg:text-4xl sm:text-lg text-3xl'>Hi! I am<span className='font-semibold'> Fatima Seemab </span></div>
+		  <div className='text-center md:text-2xl sm:text-lg text-3xl'>Student of Computer Science</div>
+		  <div className="text-center md:text-2xl sm:text-lg text-3xl">BS from National University of Science and Technology (NUST)</div>
+		  <div className="text-center md:text-2xl sm:text-lg text-3xl font-semibold">Frontend Developer</div></div>
 		</div>
   </div>;
 }
--- Components/Nav.tsx
+++ Components/Nav.tsx
@@ -9,7 +9,7 @@ function Nav() {
         <div className="max-w-7xl mx-auto  px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center">
-              <div className="flex-shrink-0 text-white font-semibold">
+              <div className="text-2xl flex-shrink-0 text-white font-semibold">
                 Fatima Portfolio
               </div>
               <div className="hidden md:block">
